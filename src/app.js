0788aa8c0ccf06e90271827c9924ed285feed951
jfiora <sillonpeludo@gmail.com>
2022-07-07 13:45:30 -0300

    setup swagger

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 import express from 'express';
-import routes from './routes/routes.js';
 import authRoutes from './routes/authRoutes.js';
 import moviesRoutes from './routes/moviesRouter.js';
 import characterRoutes from './routes/charactersRoutes.js';
 import genresRoutes from './routes/genresRoutes.js';
 import cors from 'cors';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Alkemy node challenge',
+            description: 'Disney characters and movies API',
+            contact: {
+                name: 'github.com/jfiora',
+            },
+            servers: [process.env.SERVER],
+        }
+    },
+    apis: ['./routes/*.js']
+};
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
 const app = express();
 app.use(express.json());
@@ -13,6 +29,6 @@ app.use(authRoutes);
 app.use(moviesRoutes);
 app.use(characterRoutes);
 app.use(genresRoutes);
-app.use(routes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export default app;
